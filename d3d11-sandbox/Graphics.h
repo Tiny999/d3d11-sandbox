f985e4bfc5fbeffcda39ca53273a861ec128cba5
AbbasSalami <olawalesalami3@gmail.com>
2023-07-23 22:12:57 +0100

    debug stuff

--- d3d11-sandbox/Graphics.h
+++ d3d11-sandbox/Graphics.h
@@ -1,21 +1,47 @@
 #pragma once
 #include "Win.h"
+#include "Exception.h"
 #include <d3d11.h>
+#include <wrl.h>
+#include <vector>
 
 
 class Graphics
 {
 public:
+	class GfxException : public Exception
+	{
+		using Exception::Exception;
+	};
+	class HrException : public GfxException
+	{
+	public:
+		HrException(int line, const char* file, HRESULT hr) noexcept;
+		const char* what() const noexcept override;
+		const char* GetType() const noexcept override;
+		HRESULT GetErrorCode() const noexcept;
+		std::string GetErrorString() const noexcept;
+		std::string GetErrorDescription() const noexcept;
+	private:
+		HRESULT hr;
+	};
+	class DeviceRemovedException : public HrException
+	{
+		using HrException::HrException;
+	public:
+		const char* GetType() const noexcept override;
+	};
+public:
 	Graphics(HWND hWnd);
 	Graphics(const Graphics&) = delete;
 	Graphics& operator=(const Graphics&) = delete;
-	~Graphics();
+	~Graphics() = default;
 	void EndFrame();
 	void ClearBuffer(float red, float green, float blue) noexcept;
 private:
-	ID3D11Device* pDevice = nullptr;
-	IDXGISwapChain* pSwap = nullptr;
-	ID3D11DeviceContext* pContext = nullptr;
-	ID3D11RenderTargetView* pTarget = nullptr;
+	Microsoft::WRL::ComPtr<ID3D11Device> pDevice;
+	Microsoft::WRL::ComPtr<IDXGISwapChain> pSwap;
+	Microsoft::WRL::ComPtr<ID3D11DeviceContext> pContext;
+	Microsoft::WRL::ComPtr<ID3D11RenderTargetView> pTarget;
 };
 
--- d3d11-sandbox/dxerr.h
+++ d3d11-sandbox/dxerr.h
@@ -12,7 +12,7 @@
 //--------------------------------------------------------------------------------------
 
 #pragma once
-#include "ChiliWin.h"
+#include "Win.h"
 #include <sal.h>
 
 #ifdef __cplusplus
