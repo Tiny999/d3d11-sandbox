5556f49e1f5367cdc0e2928c37eec4d99245796f
AbbasSalami <olawalesalami3@gmail.com>
2023-08-07 00:23:55 +0100

    add index buffer class

--- d3d11-sandbox/Bindable.h
+++ d3d11-sandbox/Bindable.h
@@ -1,5 +1,6 @@
 #pragma once
 #include "Graphics.h"
+#include "GraphicsThrowMacros.h"
 
 class Bindable
 {
---
+++ d3d11-sandbox/IndexBuffer.cpp
@@ -0,0 +1,31 @@
+#include "IndexBuffer.h"
+
+IndexBuffer::IndexBuffer(Graphics& gfx, std::vector<unsigned short> indices)
+	:
+	count((UINT)indices.size())
+{
+	HREFTYPE hr;
+
+	D3D11_BUFFER_DESC bd = {};
+	bd.BindFlags = D3D11_BIND_INDEX_BUFFER;
+	bd.Usage = D3D11_USAGE_DEFAULT;
+	bd.CPUAccessFlags = 0u;
+	bd.MiscFlags = 0u;
+	bd.ByteWidth = UINT(sizeof(unsigned short) * count);
+	bd.StructureByteStride = sizeof(unsigned short);
+
+	D3D11_SUBRESOURCE_DATA sd = {};
+	sd.pSysMem = indices.data();
+
+	GFX_THROW_FAILED(GetDevice(gfx)->CreateBuffer(&bd, &sd, &pIndexBuffer));
+}
+
+void IndexBuffer::Bind(Graphics& gfx) noexcept
+{
+	GetContext(gfx)->IASetIndexBuffer(pIndexBuffer.Get(), DXGI_FORMAT_R16_UINT, 0u);
+}
+
+UINT IndexBuffer::GetCount() const noexcept
+{
+	return count;
+}
