2d34ba5af53ebd27eb7425c2ab4917b6d5feddca
AbbasSalami <olawalesalami3@gmail.com>
2023-09-17 17:41:18 +0100

    scene being dynamically lit

--- d3d11-sandbox/PhongPS.hlsl
+++ d3d11-sandbox/PhongPS.hlsl
@@ -8,8 +8,8 @@ static const float3 ambient = { 0.15f, 0.15f, 0.15f };
 static const float3 diffuseColor = { 1.f, 1.f, 1.f };
 static const float diffuseIntensity = 1.0f;
 static const float attConst = 1.0f; 
-static const float attLin = 1.0f;
-static const float attQuad = 1.0f;
+static const float attLin = 0.045f;
+static const float attQuad = 0.0075f;
 
 float4 main(float3 worldPos : Position, float3 n : Normal) : SV_TARGET
 {
@@ -19,7 +19,7 @@ float4 main(float3 worldPos : Position, float3 n : Normal) : SV_TARGET
     const float3 dirToL = vToL / distToL;  
     
     // diffuse attenuation
-    const float att = attConst + attLin * distToL + attQuad * (distToL * distToL);
+    const float att = 1.0f / (attConst + attLin * distToL + attQuad * (distToL * distToL));
     
     // diffuse intensity 
     const float3 diffuse = diffuseColor * diffuseIntensity * att * max(0.0f, dot(dirToL, n));
